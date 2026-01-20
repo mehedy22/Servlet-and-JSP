1a36b8a0960289a50b1a23c54947f0bfceed6d1e
flop-signing <mehedy.mmc272@gmail.com>
2026-01-20 23:00:08 +0600

    first code using servlet

---
+++ src/main/java/com/apachekafka/servletjsp/pages/AddServlet.java
@@ -0,0 +1,10 @@
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+// This two object is created by tomcat HttpServletRequest and HttpServletResponse
+    public void service(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
