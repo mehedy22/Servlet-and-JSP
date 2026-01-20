b7ee21c414542e33ac6e097066dc5efe66e74589
flop-signing <mehedy.mmc272@gmail.com>
2026-01-21 00:04:16 +0600

    Implement addition functionality in AddServlet and update HTML form

--- src/main/java/com/apachekafka/servletjsp/pages/AddServlet.java
+++ src/main/java/com/apachekafka/servletjsp/pages/AddServlet.java
@@ -1,10 +1,21 @@
+package com.apachekafka.servletjsp.pages;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class AddServlet extends HttpServlet {
-// This two object is created by tomcat HttpServletRequest and HttpServletResponse
-    public void service(HttpServletRequest request, HttpServletResponse response) {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class AddServlet extends HttpServlet {
+    // This two object is created by tomcat HttpServletRequest and HttpServletResponse
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int i = Integer.parseInt(request.getParameter("first"));
+        int j = Integer.parseInt(request.getParameter("second"));
+
+        int sum = i + j;
+        PrintWriter out = response.getWriter();
+        out.println(sum);
     }
 }
