4a8b8fecc3e2ea190aa633e06349b1a86051c9da
flop-signing <mehedy.mmc272@gmail.com>
2026-01-22 00:15:18 +0600

    Add SquareServlet and update GetAndPostRequest to forward requests

--- src/main/java/com/apachekafka/servletjsp/pages/GetAndPostRequest.java
+++ src/main/java/com/apachekafka/servletjsp/pages/GetAndPostRequest.java
@@ -1,5 +1,7 @@
 package com.apachekafka.servletjsp.pages;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,21 +11,21 @@ import java.io.PrintWriter;
 
 public class GetAndPostRequest extends HttpServlet {
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int i = Integer.parseInt(request.getParameter("first"));
-        int j = Integer.parseInt(request.getParameter("second"));
+//    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        int i = Integer.parseInt(request.getParameter("first"));
+//        int j = Integer.parseInt(request.getParameter("second"));
+//
+//        int sum = i + j;
+//        PrintWriter out = response.getWriter();
+//        out.println(sum);
+//    }
 
-        int sum = i + j;
-        PrintWriter out = response.getWriter();
-        out.println(sum);
-    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//        int i = Integer.parseInt(request.getParameter("first"));
+//        int j = Integer.parseInt(request.getParameter("second"));
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int i = Integer.parseInt(request.getParameter("first"));
-        int j = Integer.parseInt(request.getParameter("second"));
-
-        int sum = i + j;
-        PrintWriter out = response.getWriter();
-        out.println(sum);
+        // WE wanna called the Square Servlet  from this servlet by  using Request Dispatcher
+        RequestDispatcher rd = request.getRequestDispatcher("/square");
+        rd.forward(request,response);
     }
 }
---
+++ src/main/java/com/apachekafka/servletjsp/pages/SquareServlet.java
@@ -0,0 +1,16 @@
+package com.apachekafka.servletjsp.pages;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SquareServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Hello From Square Servlet");
+    }
+}
