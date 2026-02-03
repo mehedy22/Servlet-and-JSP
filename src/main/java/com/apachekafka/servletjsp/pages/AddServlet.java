0c0283f3332459d16d92cd36477eb5e367045da5
mehedy22 <mehedy.mmc272@gmail.com>
2026-02-04 02:04:11 +0600

    Performing Square with Session Management

--- src/main/java/com/apachekafka/servletjsp/pages/AddServlet.java
+++ src/main/java/com/apachekafka/servletjsp/pages/AddServlet.java
@@ -1,10 +1,11 @@
 package com.apachekafka.servletjsp.pages;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 
 import java.io.IOException;
 
@@ -27,6 +28,10 @@ public class AddServlet extends HttpServlet {
         int j = Integer.parseInt(request.getParameter("num2"));
 
         int sum = i + j;
+        // Sending or Working with Tomcat Session Management
+
+        HttpSession session=request.getSession();
+        session.setAttribute("sum",sum);
         response.sendRedirect("square?sum=" + sum);
      /*   request.setAttribute("sum",sum);
 
--- src/main/java/com/apachekafka/servletjsp/pages/SquareServlet.java
+++ src/main/java/com/apachekafka/servletjsp/pages/SquareServlet.java
@@ -3,6 +3,7 @@ package com.apachekafka.servletjsp.pages;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,10 +13,20 @@ public class SquareServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        int k = Integer.parseInt(request.getParameter("sum"));
+   /*     int k = Integer.parseInt(request.getParameter("sum"));
+        k = k * k;
+
+        PrintWriter out = response.getWriter();
+        out.println("Result is : " + k + "");*/
+
+        // Receiving Through Session
+
+        HttpSession session = request.getSession();
+        int k = (int) session.getAttribute("sum");
         k = k * k;
 
         PrintWriter out = response.getWriter();
         out.println("Result is : " + k + "");
+
     }
 }
